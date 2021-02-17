479ed61424cf087e1161d97d45c1923be61a173b
silverwest8 <silverwest8@gmail.com>
2021-02-17 11:59:05 +0900

    layout edited

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,14 +1,14 @@
 var store = [{
-        "title": "백준 알고리즘 for JAVA",
-        "excerpt":"  [문제 사이트 바로가기]   단계별로 풀어보기   [참고링크]   참고자료   Source &amp; Comment   Chapter 1 - output   Chapter 2 - if   Chapter 3 - for   Chapter 4 - while   Chapter 5 - Array   Chapter 6 - function   Chapter 7 - String  ","categories": [],
-        "tags": ["Tips","Github io","Notion"],
-        "url": "/Algorithm/",
-        "teaser": "/assets/imges/ico.svg"
-      },{
         "title": "githubPage를 이용한 블로그 만들기",
         "excerpt":" ","categories": [],
         "tags": ["Github io"],
         "url": "/develop/",
+        "teaser": "/assets/imges/ico.svg"
+      },{
+        "title": "백준 알고리즘 for JAVA",
+        "excerpt":"  [문제 사이트 바로가기]   단계별로 풀어보기   [참고링크]   참고자료   Source &amp; Comment   Chapter 1 - output   Chapter 2 - if   Chapter 3 - for   Chapter 4 - while   Chapter 5 - Array   Chapter 6 - function   Chapter 7 - String  ","categories": [],
+        "tags": ["Tips","Github io","Notion"],
+        "url": "/algorithm/",
         "teaser": "/assets/imges/ico.svg"
       },{
         "title": "Sample Blog Post",
